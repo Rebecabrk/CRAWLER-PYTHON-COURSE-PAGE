4a13ab039e722ceeded76a1009dffbf9c9c546a9
REBECA <rebeca.costache@yahoo.com>
2024-12-14 13:31:33 +0200

    autopep8 formatted

--- HTTPxModule.py
+++ HTTPxModule.py
@@ -1,10 +1,12 @@
 import httpx
 import re
 
+
 class RequestWebPage:
     """
     Clasa care permite obtinerea continutului unei pagini web.
     """
+
     def __init__(self, url):
         """
         Constructorul clasei RequestWebPage.
@@ -21,12 +23,13 @@ class RequestWebPage:
         :param url: URL-ul care trebuie verificat.
         """
         pattern = re.compile(
-            r'^(http?|https):\/\/' # protocoale acceptate: http, https
-            r'([a-zA-Z0-9\-._~%]+|(\[[a-fA-F0-9:]+\])|(\b\d{1,3}(\.\d{1,3}){3}\b))' # domeniu sau adresa IP (v6 si v4)
-            r'([0-9]{0,5})?' # port (optional)
-            r'(\/[a-zA-Z0-9\-.~%]*)*' # cale (optional)
-            r'(\?[;&a-zA-Z0-9\-.~%=_]*)*' # query string (optional)
-            r'(\#.*)?$' # fragment (optional)
+            r'^(http?|https):\/\/'  # protocoale acceptate: http, https
+            # domeniu sau adresa IP (v6 si v4)
+            r'([a-zA-Z0-9\-._~%]+|(\[[a-fA-F0-9:]+\])|(\b\d{1,3}(\.\d{1,3}){3}\b))'
+            r'([0-9]{0,5})?'  # port (optional)
+            r'(\/[a-zA-Z0-9\-.~%]*)*'  # cale (optional)
+            r'(\?[;&a-zA-Z0-9\-.~%=_]*)*'  # query string (optional)
+            r'(\#.*)?$'  # fragment (optional)
         )
         return pattern.match(url) is not None
 
@@ -39,18 +42,22 @@ class RequestWebPage:
         if response.status_code == httpx.codes.OK:
             return response.text
         else:
-            raise ValueError(f'Error when trying to get the url: {response.reason_phrase}')
-        
+            raise ValueError(
+                f'Error when trying to get the url: {
+                    response.reason_phrase}')
+
+
 class RequestPythonCoursePage(RequestWebPage):
     """
     Clasa care permite obtinerea continutului paginii web a cursului de Python de la FII.
     """
+
     def __init__(self):
         """
         Constructorul clasei RequestPythonCoursePage.
         """
         super().__init__('https://gdt050579.github.io/python-course-fii/labs.html')
-    
+
     def get_web_page(self):
         """
         Metoda care obtine continutul paginii web a cursului de Python de la FII.
@@ -62,7 +69,7 @@ class RequestPythonCoursePage(RequestWebPage):
         Metoda care returneaza URL-ul de baza al paginii web de python de la FII.
         """
         return 'https://gdt050579.github.io/python-course-fii/'
-    
+
 
 if __name__ == '__main__':
     try:
--- Template.py
+++ Template.py
@@ -1,14 +1,18 @@
-import os, sys, re
+import os
+import sys
+import re
 from bs4 import BeautifulSoup
 from HTTPxModule import RequestWebPage, RequestPythonCoursePage
 
 exercise_lenghts = 0
 
+
 def write_function_name(statement, exercise_number, file):
     if re.search(r'\bclass\b', statement):
         pattern = r'(?<!\.\s)(\b[A-Z][a-zA-Z]*)'
         class_names = re.findall(pattern, statement)
-        class_names = [n for n in class_names if n != 'Write' and n != 'Create' and n != 'Python' and n != 'None' and n != 'Design' and n != 'NxM' and len(n) > 1]
+        class_names = [n for n in class_names if n != 'Write' and n != 'Create' and n !=
+                       'Python' and n != 'None' and n != 'Design' and n != 'NxM' and len(n) > 1]
 
         if class_names == []:
             class_names.append('Matrix')
@@ -17,7 +21,8 @@ def write_function_name(statement, exercise_number, file):
             exercise_class = f'class {class_name}:\n    pass\n'
             file.write(exercise_class)
     elif re.search(r'\bThe\s(\w+)\sfunction\b', statement):
-        function_name = re.search(r'\bThe\s(\w+)\sfunction\b', statement).group().split()[1]
+        function_name = re.search(
+            r'\bThe\s(\w+)\sfunction\b', statement).group().split()[1]
         exercise_function = f'def {function_name}():\n    pass\n'
         file.write(exercise_function)
     elif '(' in statement:
@@ -33,13 +38,15 @@ def write_function_name(statement, exercise_number, file):
         exercise_function = f'def ex{exercise_number}():\n    pass\n'
         file.write(exercise_function)
 
+
 def pretty_format_exercise_statement(statement):
     exercise_statement = f'"""\n{statement.replace("\n", "")}\n"""\n'
     words = exercise_statement.split()
-    lines = [' '.join(words[i:i+15]) for i in range(0, len(words), 15)]
+    lines = [' '.join(words[i:i + 15]) for i in range(0, len(words), 15)]
     exercise_statement = '\n'.join(lines)
     exercise_statement = exercise_statement.replace('"""', f'"""\n')
     return exercise_statement
+
 
 def parse_ol_tag(ol_tag, index, file, pre_tag=None):
     global exercise_lenghts
@@ -55,11 +62,12 @@ def parse_ol_tag(ol_tag, index, file, pre_tag=None):
             statement += ' ' + pre_tag.text
         file.write(pretty_format_exercise_statement(statement))
         write_function_name(statement, exercise_number, file)
-        
+
     if index == 0:
         exercise_lenghts = len(li)
     else:
         exercise_lenghts += len(li)
+
 
 if len(sys.argv) > 1 and sys.argv[1] == 'help':
     print('This script downloads the Python course page and all the labs from the course page.')
@@ -85,10 +93,11 @@ try:
 except ValueError as e:
     print(e)
     sys.exit(2)
-    
+
 soup = BeautifulSoup(python_webpage_content, 'html.parser')
 main = soup.find('main')
-filtered_a_tags = [a for a in main.find_all('a') if a.find_parent('h1') is None]
+filtered_a_tags = [a for a in main.find_all(
+    'a') if a.find_parent('h1') is None]
 
 base_url = python_webpage.get_base_url()
 
@@ -101,17 +110,18 @@ for lab_number, anchor in enumerate(filtered_a_tags):
 
     soup = BeautifulSoup(lab_content, 'html.parser')
     main = soup.find('main')
-   
+
     if not os.path.exists(f'lab{lab_number + 2}'):
         os.mkdir(f'lab{lab_number + 2}')
-    
+
     with open(f'lab{lab_number + 2}/lab{lab_number + 2}.py', 'w') as f:
         for index, ol_tag in enumerate(main.find_all('ol')):
-            if ol_tag.find_next_sibling('pre') is not None:   
-                parse_ol_tag(ol_tag, index, f, pre_tag=ol_tag.find_next_sibling('pre'))
+            if ol_tag.find_next_sibling('pre') is not None:
+                parse_ol_tag(ol_tag, index, f,
+                             pre_tag=ol_tag.find_next_sibling('pre'))
             else:
                 parse_ol_tag(ol_tag, index, f)
-        print('Lab', lab_number + 2, 'successfully created.') 
+        print('Lab', lab_number + 2, 'successfully created.')
 
 print()
 print('All labs successfully created.')
